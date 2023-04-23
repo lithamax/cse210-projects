0966900aa1d726580026018e04add29258078b05
lithamax <lithamaxongo@gmail.com>
2023-04-23 21:57:55 +0200

    C sharp assignment 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        if (grade >= 70)
+        {
+
+            if (grade >= 90)
+            {
+                letter = "A";
+            }  
+            else if (grade >= 80)
+            {
+                letter = "B";
+            }
+            else if (grade >= 70)
+            {
+                letter = "C";
+            }
+        Console.WriteLine($"Your grade is {letter}.");         
+        Console.WriteLine("Congratulations, you have passed!");
+        }
+        else if (grade <70)
+        {
+            if (grade >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+        Console.WriteLine($"Your grade is {letter}.");
+        Console.WriteLine("Sorry, you failed the course. Try again next time!");
+        }
+        
     }
 }
