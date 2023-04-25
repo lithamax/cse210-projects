0c293d3bf201de8c2a655bb74671015fcf28acf9
lithamax <lithamaxongo@gmail.com>
2023-04-25 03:47:09 +0200

    C sharp prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int userNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int userNumber = randomGenerator.Next(1, 101);
+        
+        int userGuess = -1;
+
+        while (userGuess != userNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess > userNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess < userNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+            Console.WriteLine("You guessed it!");
+            }            
+        
+        }
+        
+
     }
 }
