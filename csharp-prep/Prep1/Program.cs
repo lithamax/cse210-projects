a6a70fafea375f474d2bdb5f5ccc82c84e5e28b6
lithamax <lithamaxongo@gmail.com>
2023-09-17 15:09:05 +0200

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -10,6 +10,6 @@ class Program
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
