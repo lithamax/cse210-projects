b0cf21a52bb9672a9851878084eda320528c14c6
lithamax <lithamaxongo@gmail.com>
2024-03-03 20:58:41 +0200

    added minor change to surname promt

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        Console.Write("What is your last name? ");
+        Console.Write("What is your surname? ");
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
