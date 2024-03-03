830598a20051055be50bd5f6774f690fb60b35c5
lithamax <lithamaxongo@gmail.com>
2024-03-03 20:54:51 +0200

    C# prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
-        Console.Write("What is your surname? ");
+        Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
